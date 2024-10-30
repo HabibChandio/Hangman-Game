1adef1cf06e39a96c743b27fbd0825a1649bf1a8
Abdulbariarain <bari4706656@cloud.neduet.edu.pk>
2024-10-30 02:35:59 -0700

    created menu main.c

--- main.c
+++ main.c
@@ -2,7 +2,29 @@
 #include "functions.h"
 
 int main(){
-	printf("This is pojct");
-	function1();
+	int choice;
+	
+	printf("------------------------HANGMAN------------------------\n\n");
+	do{
+		printf("1. Start game\n2. View leaderboard\n3. Quit");
+		scanf("%d", &choice);		
+		
+		switch(choice) 
+		{			
+			case 1:
+				//start case
+				break;
+			case 2:
+				//leaderboard
+				break;
+			case 3:
+				printf("Thanks for playing!!!\n");
+				return 0;
+				break;
+			default:
+				printf("Incorrect input!\n");
+				continue;			
+		}
+	}while(//);
 	return 0;
 }
