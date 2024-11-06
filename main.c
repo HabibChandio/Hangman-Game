0cbb815b24cf934f032f3e92c07bc06ead00d30d
Abdulbariarain <bari4706656@cloud.neduet.edu.pk>
2024-11-06 04:33:17 -0800

    Update main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "functions.h"
+
+int main()
+{
+	int choice;
+	
+	printf("-------------WELCOME TO HANGMAN -------------\n\n");
+	
+	printf("1. Start\n2. Leaderboard\n3. Quit");
+	scanf("%d",&choice);
+	do{
+		if(choice == 1) {
+			//
+		}
+		else if(choice == 2) 
+			//
+		else if(choice == 3)
+			//
+		else
+			printf("Invalid input");
+	  }while(choice != 3)
+	return 0;
+}
